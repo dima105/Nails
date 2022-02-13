298df459bfb2c40558a32fca2899745cc52a67ea
Dmitry_Kulik <dima27121985@gmail.com>
2022-02-13 19:03:21 +0300

    Добавьте файлы проекта.

---
+++ Nails/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace nails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Количество гвоздей: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Количество ударов: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+            int z;
+
+            while (x > 0)
+            {
+                x--;
+                z = y;
+                while (z > 0)
+                {
+                    z--;
+                }
+            }
+        }
+    }
+}
